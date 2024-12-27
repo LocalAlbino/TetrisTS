2790d919fd6e4cc17efdcd78dce6bf8108997722
LocalAlbino <rudyhartwig25@outlook.com>
2024-12-27 02:51:45 -0500

    refactor: used 1d array for matrix to simplify game

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-import "./block.ts";
-import { BlockType, getBlock, Piece } from "./block.ts";
-
-const rows = 40,
-  cols = 10;
+const cells = 400;
 
 // Type assertion here since these elements always should exist
 const c = document.getElementById("matrix") as HTMLCanvasElement;
 const ctx = c.getContext("2d") as CanvasRenderingContext2D;
 
-let matrix = Array(rows).fill(Array(cols).fill(0));
+let matrix = Array(cells).fill(0);
 
 function randomGenerator() {
   // Fisher-Yates shuffle algorithm will generate a randomly shuffled set of blocks
@@ -30,7 +26,7 @@ function randomGenerator() {
 
 function setColor(block: number, ctx: CanvasRenderingContext2D) {
   if (block === 0) {
-    ctx.fillStyle = "block";
+    ctx.fillStyle = "black";
   } else if (block === 1) {
     ctx.fillStyle = "cyan";
   } else if (block === 2) {
@@ -48,34 +44,61 @@ function setColor(block: number, ctx: CanvasRenderingContext2D) {
   } else {
     // Reset to default state if number falls outside of range
     block = 0;
-    ctx.fillStyle = "block";
+    ctx.fillStyle = "black";
   }
 }
 
-function draw() {
-  const skyline = 18,
-    spacing = 10,
-    halfSpacing = 5;
-  for (let row = skyline; row < rows; row++) {
-    for (let col = 0; col < cols; col++) {
-      if (row === skyline) {
-        // Row is the half-line between the skyline and bufferzone
-        // Therefore the tiles should only be half the height
-        setColor(matrix[row][col], ctx);
-        ctx.fillRect(col * spacing, 0, spacing, halfSpacing);
-      } else {
-        // Normal matrix rows, should have full spacing
-        setColor(matrix[row][col], ctx);
-        ctx.fillRect(
-          col * spacing,
-          (row - skyline - 1) * spacing + halfSpacing,
-          spacing,
-          spacing,
-        );
-      }
+function draw(ctx: CanvasRenderingContext2D) {
+  const spacing = 10,
+    halfSpacing = 5,
+    skyline = 190;
+
+  for (let cell = skyline; cell < cells; cell++) {
+    const row = calculateRow(cell),
+      column = calculateColumn(cell);
+
+    // row * spacing returns a value between [0, 390] to get the exact cell
+    setColor(matrix[cell], ctx);
+    if (cell < skyline + spacing) {
+      ctx.fillRect(
+        column * spacing,
+        row * spacing - skyline,
+        spacing,
+        halfSpacing,
+      );
+    } else {
+      ctx.fillRect(
+        column * spacing,
+        row * spacing - skyline - halfSpacing,
+        spacing,
+        spacing,
+      );
     }
   }
   ctx.fill();
 }
 
-draw();
+function calculateRow(cell: number) {
+  // Splits the cell into an array of digits
+  // The first one or two digits can be used to calculate the row
+  // The range of cells is [0, 399]
+  let digits = cell.toString().split("").map(Number);
+  if (digits.length > 2) {
+    return digits[0] * 10 + digits[1]; // The first digit is the tens and the second is the ones
+  } else if (digits.length > 1) {
+    return digits[0]; // The first digit is the row
+  } else {
+    return 0; // If there is no first digit, this zero is implied
+  }
+}
+
+function calculateColumn(cell: number) {
+  // Splits the cell into an array of digits
+  // The last digit can be used to calculate the column
+  // The range of cells is [0, 399]
+  let digits = cell.toString().split("").map(Number);
+
+  return digits[digits.length - 1];
+}
+
+draw(ctx);
